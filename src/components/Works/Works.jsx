fd246a293813d209fd6497d4d6c019f9a093e931
AYUSH KUMAR <ak0735000@gmail.com>
2022-06-09 17:51:15 +0530

    update

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -36,7 +36,7 @@ const Works = () => {
             ispum is simpley dummy text of printing
           </spane> */}
           <Link to="contact" smooth={true} spy={true}>
-            <button className="button s-button">Hire Me</button>
+            {/* <button className="button s-button">Hire Me</button> */}
           </Link>
           <div
             className="blur s-blur1"
